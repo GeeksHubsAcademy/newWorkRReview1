bc30aff05939f5fc283dc9d4849f6e3262bbc3ce
Dave86dev <davidochando1986@gmail.com>
2022-11-24 14:05:42 +0100

    locations search implemented

---
+++ src/components/LocationCard/LocationCard.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './LocationCard.css';
+ 
+const LocationCard = ({location}) => {
+     return (
+         <div className='locationCardDesign'>
+            
+
+         </div>
+     )
+}
+export default LocationCard;
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -13,3 +13,13 @@ export const bringCharacters = async () => {
     return await axios(config);
 };
 
+export const bringLocation = async (search) => {
+
+    let config = {
+        method: 'get',
+        url: `${root}/location/${search}`
+    }
+
+    return await axios(config);
+}
+
